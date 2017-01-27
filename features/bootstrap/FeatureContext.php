bae359c650f7ba67ca231bf2da2def58289673e7
Stephen Harris <contact@stephenharris.info>
2017-01-27 11:07:36 +0000

    Attempt to upload artefacts to S3

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -9,12 +9,33 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 class FeatureContext extends RawWordpressContext implements SnippetAcceptingContext {
 
     /**
-     * Initialise context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the context constructor through behat.yml.
+     * Location to store screenshots, or false if none are to be taken
+     * @var string|bool
      */
-    public function __construct() {
+    protected $screenshot_dir = false;
+
+    public function __construct($screenshot_dir=false) {
+        if ( $screenshot_dir ) {
+            $this->screenshot_dir = rtrim( $screenshot_dir, '/' ) . '/';
+        }
         parent::__construct();
+    }
+
+    /**
+     * @AfterScenario
+     */
+    public function takeScreenshotAfterFailedStep(AfterScenarioScope $scope)
+    {
+        if ($this->screenshot_dir) {
+            $feature  = $scope->getFeature();
+            $scenario = $scope->getScenario();
+            $filename = basename( $feature->getFile(), '.feature' ) . '-' . $scenario->getLine();
+            if ($this->getSession()->getDriver() instanceof \Behat\Mink\Driver\Selenium2Driver) {
+                $screenshot = $this->getSession()->getDriver()->getScreenshot();
+                file_put_contents( $this->screenshot_dir . $filename . '.png', $screenshot);
+            }
+            //Store HTML markup of the page also - useful for non-js tests
+            file_put_contents( $this->screenshot_dir . $filename . '.html', $this->getSession()->getPage()->getHtml());
+        }
     }
 }
