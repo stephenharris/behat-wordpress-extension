2f2ebaea673dc40b841e780e4b6b8b7f0398f483
Paul Gibbs <djpaul@gmail.com>
2017-04-22 12:02:43 +0100

    Driver elements, WP-API: add Comment element.

---
+++ src/Driver/Element/Wpapi/CommentElement.php
@@ -0,0 +1,63 @@
+<?php
+namespace PaulGibbs\WordpressBehatExtension\Driver\Element\Wpapi;
+
+use PaulGibbs\WordpressBehatExtension\Driver\Element\BaseElement;
+use UnexpectedValueException;
+
+/**
+ * WP-API driver element for post comments.
+ */
+class CommentElement extends BaseElement
+{
+    /**
+     * Create an item for this element.
+     *
+     * @param array $args Data used to create an object.
+     *
+     * @return \WP_Comment The new item.
+     */
+    public function create($args)
+    {
+        $comment_id = wp_new_comment($args);
+
+        if (! $comment_id || is_wp_error($comment_id)) {
+            throw new UnexpectedValueException('WordPress API driver failed creating a new comment.');
+        }
+
+        return $this->get($comment_id);
+    }
+
+    /**
+     * Retrieve an item for this element.
+     *
+     * @param int|string $id   Object ID.
+     * @param array      $args Not used.
+     *
+     * @return \WP_Comment The item.
+     */
+    public function get($id, $args = [])
+    {
+        $comment = get_comment($id);
+
+        if (! $comment) {
+            throw new UnexpectedValueException(sprintf('Could not find comment with ID %d', $id));
+        }
+
+        return $comment;
+    }
+
+    /**
+     * Delete an item for this element.
+     *
+     * @param int|string $id   Object ID.
+     * @param array      $args Optional data used to delete an object.
+     */
+    public function delete($id, $args = [])
+    {
+        $result = wp_delete_comment($id, isset($args['force']));
+
+        if (! $result) {
+            throw new UnexpectedValueException('WordPress API driver failed deleting a comment.');
+        }
+    }
+}
